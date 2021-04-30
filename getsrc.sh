fb36d34c163954e7d43dd4127d1b3b1d8cd1b33a
Jeb Rosen <jeb.rosen@isbscience.org>
2021-04-30 11:37:27 -0700

    change(getsrc.sh): download sources over https where possible

--- getsrc.sh
+++ getsrc.sh
@@ -23,18 +23,18 @@ download() {
 
 mkdir -p src
 
-download http://www.repeatmasker.org/rmblast-2.11.0+-x64-linux.tar.gz
+download https://www.repeatmasker.org/rmblast-2.11.0+-x64-linux.tar.gz
 download http://eddylab.org/software/hmmer/hmmer-3.3.2.tar.gz
 download https://github.com/Benson-Genomics-Lab/TRF/archive/v4.09.1.tar.gz trf-4.09.1.tar.gz
-download http://www.repeatmasker.org/RepeatScout-1.0.6.tar.gz
-download http://www.repeatmasker.org/RepeatModeler/RECON-1.08.tar.gz
+download https://www.repeatmasker.org/RepeatScout-1.0.6.tar.gz
+download https://www.repeatmasker.org/RepeatModeler/RECON-1.08.tar.gz
 download https://github.com/weizhongli/cdhit/releases/download/V4.8.1/cd-hit-v4.8.1-2019-0228.tar.gz
 download https://github.com/genometools/genometools/archive/v1.6.0.tar.gz gt-1.6.0.tar.gz
 download https://github.com/oushujun/LTR_retriever/archive/v2.9.0.tar.gz LTR_retriever-2.9.0.tar.gz
 download https://mafft.cbrc.jp/alignment/software/mafft-7.471-without-extensions-src.tgz
 download https://github.com/TravisWheelerLab/NINJA/archive/0.97-cluster_only.tar.gz NINJA-cluster.tar.gz
-download http://www.repeatmasker.org/coseg-0.2.2.tar.gz
-download http://www.repeatmasker.org/RepeatMasker/RepeatMasker-4.1.2-p1.tar.gz
+download https://www.repeatmasker.org/coseg-0.2.2.tar.gz
+download https://www.repeatmasker.org/RepeatMasker/RepeatMasker-4.1.2-p1.tar.gz
 download https://github.com/Dfam-consortium/RepeatModeler/archive/2.0.2a.tar.gz RepeatModeler-2.0.2a.tar.gz
 
 # TODO: /exe/ only includes binaries of the "latest" version at the time of download.
@@ -42,5 +42,5 @@ download https://github.com/Dfam-consortium/RepeatModeler/archive/2.0.2a.tar.gz 
 # On whatever was downloaded.
 # Consider building these tools from source instead.
 for tool in faToTwoBit twoBitInfo twoBitToFa; do
-  download http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/"$tool"
+  download https://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/"$tool"
 done
